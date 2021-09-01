9aa8454ea1b0fa052b71feca67f27282ea7ae2d6
GersonD <gmatostoribio@gmail.com>
2021-09-01 11:25:39 -0500

    css modificado header para celulares

--- components/cabecera.tsx
+++ components/cabecera.tsx
@@ -10,40 +10,54 @@ const Cabecera = () => {
 
   const handleMenu = () => setActiveMenu(!isActiveMenu);
 
+  const IraInicio  = () => window.location.href = '/' 
+
+
   return (
     <div className={styles.cabecera}>
       <div className={styles.content_logo}>
-        <div className={styles.logo}>
-          <Image
-            priority
-            src="/images/logo_kemay_antena.png"
-            height={50}
-            width={68}
-            alt="KemayTechnology"
-          />
+        <div className={styles.div_logo_title}>
+          <div className={styles.logo} onClick={IraInicio}>
+            
+            <Image
+              priority
+              src="/images/logo_kemay_antena.png"
+              height={50}
+              width={58}
+              alt="KemayTechnology"
+            />
+          </div>
+          <div className={styles.title_logo}>
+            <strong>KEMAYTECHNOLOGY</strong>
+          </div>
         </div>
-        <div className={styles.title_logo}>Kemaytechnology</div>
+       
+        <div className={styles.div_iconbar}>
         <div className={styles.iconbar}>
           <FontAwesomeIcon onClick={handleMenu} size="lg" icon={faBars} />
+        </div>
         </div>
       </div>
       {isActiveMenu && (
         <div className={styles.menu_cel}>
           <div className={styles.content_menu_cel}>
             <ul>
-              <li>
-                <Link href="/">
-                  <a>Inicio</a>
+              <li className={styles.li_menu_celulares} >
+              <Link href="/">
+                <a className={styles.menu_celulares}>
+                  Inicio
+                </a>
                 </Link>
               </li>
-              <li>
+
+              <li className={styles.li_menu_celulares}>
                 <Link href="/precios">
-                  <a>Precios</a>
+                  <a className={styles.menu_celulares}>Precios</a>
                 </Link>
               </li>
-              <li>
+              <li className={styles.li_menu_celulares}>
                 <Link href="/contactanos">
-                  <a>Contactanos</a>
+                  <a className={styles.menu_celulares}>Contactanos</a>
                 </Link>
               </li>
             </ul>
